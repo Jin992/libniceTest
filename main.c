8c4736dc42da68bf7d16b425ab40995682cff26c
Yevhen Arteshchuk <arteshchuk@gmail.com>
2019-09-20 11:54:22 +0300

    third attempt
    Signed-off-by: Yevhen Arteshchuk <arteshchuk@gmail.com>

--- main.c
+++ main.c
@@ -31,11 +31,11 @@
  * firewalls.
  *
  * Build:
- *   gcc -o sdp-example sdp-example.c `pkg-config --cflags --libs nice`
+ *   gcc -o threaded-example threaded-example.c `pkg-config --cflags --libs nice`
  *
  * Run two clients, one controlling and one controlled:
- *   sdp-example 0 $(host -4 -t A stun.stunprotocol.org | awk '{ print $4 }')
- *   sdp-example 1 $(host -4 -t A stun.stunprotocol.org | awk '{ print $4 }')
+ *   threaded-example 0 $(host -4 -t A stun.stunprotocol.org | awk '{ print $4 }')
+ *   threaded-example 1 $(host -4 -t A stun.stunprotocol.org | awk '{ print $4 }')
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -54,11 +54,20 @@ static gboolean exit_thread, candidate_gathering_done, negotiation_done;
 static GMutex gather_mutex, negotiate_mutex;
 static GCond gather_cond, negotiate_cond;
 
+static const gchar *candidate_type_name[] = {"host", "srflx", "prflx", "relay"};
+
 static const gchar *state_name[] = {"disconnected", "gathering", "connecting",
                                     "connected", "ready", "failed"};
 
+static int print_local_data(NiceAgent *agent, guint stream_id,
+                            guint component_id);
+static int parse_remote_data(NiceAgent *agent, guint stream_id,
+                             guint component_id, char *line);
 static void cb_candidate_gathering_done(NiceAgent *agent, guint stream_id,
                                         gpointer data);
+static void cb_new_selected_pair(NiceAgent *agent, guint stream_id,
+                                 guint component_id, gchar *lfoundation,
+                                 gchar *rfoundation, gpointer data);
 static void cb_component_state_changed(NiceAgent *agent, guint stream_id,
                                        guint component_id, guint state,
                                        gpointer data);
@@ -113,17 +122,18 @@ static void *
 example_thread(void *data)
 {
     NiceAgent *agent;
+    NiceCandidate *local, *remote;
     GIOChannel* io_stdin;
     guint stream_id;
     gchar *line = NULL;
-    gchar *sdp, *sdp64;
+    int rval;
 
 #ifdef G_OS_WIN32
     io_stdin = g_io_channel_win32_new_fd(_fileno(stdin));
 #else
     io_stdin = g_io_channel_unix_new(fileno(stdin));
 #endif
-    g_io_channel_set_flags(io_stdin, G_IO_FLAG_NONBLOCK, NULL);
+    g_io_channel_set_flags (io_stdin, G_IO_FLAG_NONBLOCK, NULL);
 
     // Create the nice agent
     agent = nice_agent_new(g_main_loop_get_context (gloop),
@@ -141,6 +151,8 @@ example_thread(void *data)
     // Connect to the signals
     g_signal_connect(agent, "candidate-gathering-done",
                      G_CALLBACK(cb_candidate_gathering_done), NULL);
+    g_signal_connect(agent, "new-selected-pair",
+                     G_CALLBACK(cb_new_selected_pair), NULL);
     g_signal_connect(agent, "component-state-changed",
                      G_CALLBACK(cb_component_state_changed), NULL);
 
@@ -148,7 +160,6 @@ example_thread(void *data)
     stream_id = nice_agent_add_stream(agent, 1);
     if (stream_id == 0)
         g_error("Failed to add stream");
-    nice_agent_set_stream_name (agent, stream_id, "text");
 
     // Attach to the component to receive the data
     // Without this call, candidates cannot be gathered
@@ -169,13 +180,10 @@ example_thread(void *data)
         goto end;
 
     // Candidate gathering is done. Send our local candidates on stdout
-    sdp = nice_agent_generate_local_sdp (agent);
-    printf("Generated SDP from agent :\n%s\n\n", sdp);
-    printf("Copy the following line to remote client:\n");
-    sdp64 = g_base64_encode ((const guchar *)sdp, strlen (sdp));
-    printf("\n  %s\n", sdp64);
-    g_free (sdp);
-    g_free (sdp64);
+    printf("Copy this line to remote client:\n");
+    printf("\n  ");
+    print_local_data(agent, stream_id, 1);
+    printf("\n");
 
     // Listen on stdin for the remote candidate list
     printf("Enter remote data (single line, no wrapping):\n");
@@ -184,12 +192,9 @@ example_thread(void *data)
     while (!exit_thread) {
         GIOStatus s = g_io_channel_read_line (io_stdin, &line, NULL, NULL, NULL);
         if (s == G_IO_STATUS_NORMAL) {
-            gsize sdp_len;
-
-            sdp = (gchar *) g_base64_decode (line, &sdp_len);
             // Parse remote candidate list and set it on the agent
-            if (sdp && nice_agent_parse_remote_sdp (agent, sdp) > 0) {
-                g_free (sdp);
+            rval = parse_remote_data(agent, stream_id, 1, line);
+            if (rval == EXIT_SUCCESS) {
                 g_free (line);
                 break;
             } else {
@@ -198,7 +203,6 @@ example_thread(void *data)
                 printf("> ");
                 fflush (stdout);
             }
-            g_free (sdp);
             g_free (line);
         } else if (s == G_IO_STATUS_AGAIN) {
             g_usleep (100000);
@@ -213,13 +217,24 @@ example_thread(void *data)
     if (exit_thread)
         goto end;
 
+    // Get current selected candidate pair and print IP address used
+    if (nice_agent_get_selected_pair (agent, stream_id, 1,
+                                      &local, &remote)) {
+        gchar ipaddr[INET6_ADDRSTRLEN];
+
+        nice_address_to_string(&local->addr, ipaddr);
+        printf("\nNegotiation complete: ([%s]:%d,",
+               ipaddr, nice_address_get_port(&local->addr));
+        nice_address_to_string(&remote->addr, ipaddr);
+        printf(" [%s]:%d)\n", ipaddr, nice_address_get_port(&remote->addr));
+    }
+
     // Listen to stdin and send data written to it
     printf("\nSend lines to remote (Ctrl-D to quit):\n");
     printf("> ");
     fflush (stdout);
     while (!exit_thread) {
         GIOStatus s = g_io_channel_read_line (io_stdin, &line, NULL, NULL, NULL);
-
         if (s == G_IO_STATUS_NORMAL) {
             nice_agent_send(agent, stream_id, 1, strlen(line), line);
             g_free (line);
@@ -235,8 +250,8 @@ example_thread(void *data)
     }
 
     end:
-    g_object_unref(agent);
     g_io_channel_unref (io_stdin);
+    g_object_unref(agent);
     g_main_loop_quit (gloop);
 
     return NULL;
@@ -272,6 +287,15 @@ cb_component_state_changed(NiceAgent *agent, guint stream_id,
     }
 }
 
+
+static void
+cb_new_selected_pair(NiceAgent *agent, guint stream_id,
+                     guint component_id, gchar *lfoundation,
+                     gchar *rfoundation, gpointer data)
+{
+    g_debug("SIGNAL: selected pair %s %s", lfoundation, rfoundation);
+}
+
 static void
 cb_nice_recv(NiceAgent *agent, guint stream_id, guint component_id,
              guint len, gchar *buf, gpointer data)
@@ -281,4 +305,157 @@ cb_nice_recv(NiceAgent *agent, guint stream_id, guint component_id,
 
     printf("%.*s", len, buf);
     fflush(stdout);
+}
+
+static NiceCandidate *
+parse_candidate(char *scand, guint stream_id)
+{
+    NiceCandidate *cand = NULL;
+    NiceCandidateType ntype;
+    gchar **tokens = NULL;
+    guint i;
+
+    tokens = g_strsplit (scand, ",", 5);
+    for (i = 0; tokens[i]; i++);
+    if (i != 5)
+        goto end;
+
+    for (i = 0; i < G_N_ELEMENTS (candidate_type_name); i++) {
+        if (strcmp(tokens[4], candidate_type_name[i]) == 0) {
+            ntype = i;
+            break;
+        }
+    }
+    if (i == G_N_ELEMENTS (candidate_type_name))
+        goto end;
+
+    cand = nice_candidate_new(ntype);
+    cand->component_id = 1;
+    cand->stream_id = stream_id;
+    cand->transport = NICE_CANDIDATE_TRANSPORT_UDP;
+    strncpy(cand->foundation, tokens[0], NICE_CANDIDATE_MAX_FOUNDATION);
+    cand->foundation[NICE_CANDIDATE_MAX_FOUNDATION - 1] = 0;
+    cand->priority = atoi (tokens[1]);
+
+    if (!nice_address_set_from_string(&cand->addr, tokens[2])) {
+        g_message("failed to parse addr: %s", tokens[2]);
+        nice_candidate_free(cand);
+        cand = NULL;
+        goto end;
+    }
+
+    nice_address_set_port(&cand->addr, atoi (tokens[3]));
+
+    end:
+    g_strfreev(tokens);
+
+    return cand;
+}
+
+
+static int
+print_local_data (NiceAgent *agent, guint stream_id, guint component_id)
+{
+    int result = EXIT_FAILURE;
+    gchar *local_ufrag = NULL;
+    gchar *local_password = NULL;
+    gchar ipaddr[INET6_ADDRSTRLEN];
+    GSList *cands = NULL, *item;
+
+    if (!nice_agent_get_local_credentials(agent, stream_id,
+                                          &local_ufrag, &local_password))
+        goto end;
+
+    cands = nice_agent_get_local_candidates(agent, stream_id, component_id);
+    if (cands == NULL)
+        goto end;
+
+    printf("%s %s", local_ufrag, local_password);
+
+    for (item = cands; item; item = item->next) {
+        NiceCandidate *c = (NiceCandidate *)item->data;
+
+        nice_address_to_string(&c->addr, ipaddr);
+
+        // (foundation),(prio),(addr),(port),(type)
+        printf(" %s,%u,%s,%u,%s",
+               c->foundation,
+               c->priority,
+               ipaddr,
+               nice_address_get_port(&c->addr),
+               candidate_type_name[c->type]);
+    }
+    printf("\n");
+    result = EXIT_SUCCESS;
+
+    end:
+    if (local_ufrag)
+        g_free(local_ufrag);
+    if (local_password)
+        g_free(local_password);
+    if (cands)
+        g_slist_free_full(cands, (GDestroyNotify)&nice_candidate_free);
+
+    return result;
+}
+
+
+static int
+parse_remote_data(NiceAgent *agent, guint stream_id,
+                  guint component_id, char *line)
+{
+    GSList *remote_candidates = NULL;
+    gchar **line_argv = NULL;
+    const gchar *ufrag = NULL;
+    const gchar *passwd = NULL;
+    int result = EXIT_FAILURE;
+    int i;
+
+    line_argv = g_strsplit_set (line, " \t\n", 0);
+    for (i = 0; line_argv && line_argv[i]; i++) {
+        if (strlen (line_argv[i]) == 0)
+            continue;
+
+        // first two args are remote ufrag and password
+        if (!ufrag) {
+            ufrag = line_argv[i];
+        } else if (!passwd) {
+            passwd = line_argv[i];
+        } else {
+            // Remaining args are serialized canidates (at least one is required)
+            NiceCandidate *c = parse_candidate(line_argv[i], stream_id);
+
+            if (c == NULL) {
+                g_message("failed to parse candidate: %s", line_argv[i]);
+                goto end;
+            }
+            remote_candidates = g_slist_prepend(remote_candidates, c);
+        }
+    }
+    if (ufrag == NULL || passwd == NULL || remote_candidates == NULL) {
+        g_message("line must have at least ufrag, password, and one candidate");
+        goto end;
+    }
+
+    if (!nice_agent_set_remote_credentials(agent, stream_id, ufrag, passwd)) {
+        g_message("failed to set remote credentials");
+        goto end;
+    }
+
+    // Note: this will trigger the start of negotiation.
+    if (nice_agent_set_remote_candidates(agent, stream_id, component_id,
+                                         remote_candidates) < 1) {
+        g_message("failed to set remote candidates");
+        goto end;
+    }
+
+    result = EXIT_SUCCESS;
+
+    end:
+    if (line_argv != NULL)
+        g_strfreev(line_argv);
+    if (remote_candidates != NULL)
+        g_slist_free_full(remote_candidates, (GDestroyNotify)&nice_candidate_free);
+
+    return result;
 }
